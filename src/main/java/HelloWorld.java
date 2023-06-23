6f0645f19badfb833163ccd7418d39d55421fb01
deepeshtelkar <31217469+deepeshtelkar@users.noreply.github.com>
2023-06-23 18:45:28 -0400

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Devapps IT");
+        System.out.println("Happy Independence day");
     }
 }
